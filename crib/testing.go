da310a14aac10803c66e4ea00daf5eed17bb881f
ajgrande924 <ajgrande924@gmail.com>
2025-07-18 17:59:10 -0400

    chore: fix

--- crib/testing.go
+++ crib/testing.go
@@ -7,6 +7,8 @@ import (
 	"github.com/smartcontractkit/crib-sdk/internal"
 )
 
+var GlobalJSIIKernelMutex = &JSIIKernelMutex{}
+
 // JSIIKernelMutex is global mutex to synchronize all parallel invocations of jsii kernel
 // This is required to be used in Tests when test code contains any invocations to jsii kernel and uses t.Parallel()
 // It can be also used in the productions code if it contains any parallel invocations.
