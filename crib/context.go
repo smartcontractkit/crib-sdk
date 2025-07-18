d83124d716f96130e66984f99fe7c3d98d55b53b
ajgrande924 <ajgrande924@gmail.com>
2025-07-18 17:00:50 -0400

    feat: create private composite

--- crib/context.go
+++ crib/context.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"github.com/aws/constructs-go/constructs/v10"
-
 	"github.com/smartcontractkit/crib-sdk/internal"
 )
 
@@ -16,4 +15,14 @@ func ConstructFromContext(ctx context.Context) constructs.Construct {
 // ContextWithConstruct creates a new context with supplied constructs.Construct value.
 func ContextWithConstruct(ctx context.Context, c constructs.Construct) context.Context {
 	return internal.ContextWithConstruct(ctx, c)
+}
+
+// ValidatorFromContext retrieves the validator from the context. If one does not exist, it is created.
+func ValidatorFromContext(ctx context.Context) *internal.Validator {
+	return internal.ValidatorFromContext(ctx)
+}
+
+// ContextWithValidator creates a new context with the supplied validator value.
+func ContextWithValidator(ctx context.Context, v *internal.Validator) context.Context {
+	return internal.ContextWithValidator(ctx, v)
 }
--- crib/testing.go
+++ crib/testing.go
@@ -10,6 +10,11 @@ import (
 // TestApp exposes the cdk8s.App and cdk8s.Chart types for use in unit tests.
 type TestApp = internal.TestApp
 
+// JSIIKernelMutex is global mutex to synchronize all parallel invocations of jsii kernel
+// This is required to be used in Tests when test code contains any invocations to jsii kernel and uses t.Parallel()
+// It can be also used in the productions code if it contains any parallel invocations.
+var JSIIKernelMutex = &internal.JSIIKernelMutex
+
 // NewHelmValuesLoader is a helper function to create a loader capable of loading values from a Helm Chart values file.
 func NewHelmValuesLoader(ctx context.Context, path string) (*internal.FileLoader, error) {
 	return internal.NewHelmValuesLoader(ctx, path)
