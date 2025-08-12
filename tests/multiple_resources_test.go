2dc0c2c194f7e80358ea86241a15008366c4dfdf
Peter Olds <me@polds.dev>
2025-08-12 13:57:43 -0700

    test: Autosnap on tests

--- tests/multiple_resources_test.go
+++ tests/multiple_resources_test.go
@@ -1,11 +1,9 @@
 package tests
 
 import (
-	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 
 	"github.com/smartcontractkit/crib-sdk/internal"
 
@@ -16,22 +14,18 @@ func TestMultipleNamespaces(t *testing.T) {
 	t.Parallel()
 	is := assert.New(t)
 
-	goldenFile, err := os.ReadFile("testdata/multiple_namespaces.golden.yaml")
-	require.NoError(t, err)
-
 	app := internal.NewTestApp(t)
-	ctx := internal.ContextWithConstruct(t.Context(), app.Chart)
 	namespaces := []string{"foo", "bar", "baz"}
 	for _, ns := range namespaces {
 		t.Run(ns, func(t *testing.T) {
 			// Note: Do not use t.Parallel() here. It will cause indeterministic output.
 
 			props := &namespace.Props{Namespace: ns}
-			component, err := namespace.New(ctx, props)
+			component, err := namespace.New(app.Context(), props)
 			is.NoError(err)
 			is.NotNil(component)
 		})
 	}
 
-	is.Equal(string(goldenFile), *app.SynthYaml())
+	app.SynthYaml()
 }
